ea39b4da77fdec489c9068cd4d8f4a1e37148f03
Hamilton Lopez <hamilt727@gmail.com>
2025-05-31 15:05:09 -0500

    update

--- src/test/java/com/example/studentscreate/service/StudentCreateServiceTest.java
+++ src/test/java/com/example/studentscreate/service/StudentCreateServiceTest.java
@@ -1,19 +1,7 @@
 package com.example.studentscreate.service;
 
-/*import com.example.studentscreate.model.Student;
-import com.example.studentscreate.repository.StudentRepository;*/
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
 public class StudentCreateServiceTest {
 /*
     @Mock
